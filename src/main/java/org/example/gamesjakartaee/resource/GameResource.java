979be9b596acaccdb65be70d2617de1c584a9d55
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-02-24 22:19:58 +0100

    Edit new path and hello method now produce JSON

--- src/main/java/org/example/gamesjakartaee/resource/GameResource.java
+++ src/main/java/org/example/gamesjakartaee/resource/GameResource.java
@@ -3,12 +3,14 @@ package org.example.gamesjakartaee.resource;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.example.gamesjakartaee.dto.GameDTO;
 
-@Path("/hello-world")
+@Path("/games")
 public class GameResource {
     @GET
-    @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    @Produces(MediaType.APPLICATION_JSON)
+    public GameDTO hello() {
+        return new GameDTO("God of war",2005);
     }
 }
