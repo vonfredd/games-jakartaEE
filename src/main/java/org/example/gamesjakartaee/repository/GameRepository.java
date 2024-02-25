bd26b4d338e1d9f651568657278b185886257c33
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-02-25 21:01:31 +0100

    Add entitymanager and method to add game and get all

--- src/main/java/org/example/gamesjakartaee/repository/GameRepository.java
+++ src/main/java/org/example/gamesjakartaee/repository/GameRepository.java
@@ -1,8 +1,29 @@
 package org.example.gamesjakartaee.repository;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.example.gamesjakartaee.dto.GameDTO;
+import org.example.gamesjakartaee.entity.Game;
+
+import java.util.List;
 
 @ApplicationScoped
 public class GameRepository {
+
+    @PersistenceContext(name = "mysql")
+    private EntityManager entityManager;
+
+    public List<GameDTO> findAll(){
+        return entityManager.createNamedQuery("GameEntity.findAll", GameDTO.class)
+                .getResultList();
+    }
+
+    @Transactional
+    public void insertGame(GameDTO gameDTO) {
+        Game game = GameDTO.map(gameDTO);
+        entityManager.persist(game);
+    }
 
 }
