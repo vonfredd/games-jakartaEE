1d4390b277ccf0b7412efc8713478fb65e92d5ad
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-02-26 14:14:43 +0100

    Edit id type to UUID

--- src/main/java/org/example/gamesjakartaee/repository/GameRepository.java
+++ src/main/java/org/example/gamesjakartaee/repository/GameRepository.java
@@ -8,6 +8,7 @@ import org.example.gamesjakartaee.dto.GameDTO;
 import org.example.gamesjakartaee.entity.Game;
 
 import java.util.List;
+import java.util.UUID;
 
 @ApplicationScoped
 public class GameRepository {
@@ -21,7 +22,7 @@ public class GameRepository {
     }
 
     @Transactional
-    public Long insertGame(GameDTO gameDTO) {
+    public UUID insertGame(GameDTO gameDTO) {
         Game game = GameDTO.map(gameDTO);
         entityManager.persist(game);
         return game.getId();
