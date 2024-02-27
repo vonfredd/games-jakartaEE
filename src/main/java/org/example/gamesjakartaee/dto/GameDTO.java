e65bb618df93bdbb5b40bcca2defe639faa14994
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-02-27 14:09:50 +0100

    Add validation annotation for parameters

--- src/main/java/org/example/gamesjakartaee/dto/GameDTO.java
+++ src/main/java/org/example/gamesjakartaee/dto/GameDTO.java
@@ -1,10 +1,12 @@
 package org.example.gamesjakartaee.dto;
 
 import org.example.gamesjakartaee.entity.Game;
+import org.example.gamesjakartaee.validate.CustomReleaseDate;
+import org.example.gamesjakartaee.validate.NotNullEmpty;
 
-public record GameDTO(String name, int releaseDate) {
-    public static GameDTO map (Game game){
-        return new GameDTO(game.getName(), game.getReleaseDate());
+public record GameDTO(@NotNullEmpty String name, @CustomReleaseDate int releaseDate) {
+
+    public static GameDTO map (Game game){return new GameDTO(game.getName(), game.getReleaseDate());
     }
 
     public static Game map (GameDTO gameDTO){
