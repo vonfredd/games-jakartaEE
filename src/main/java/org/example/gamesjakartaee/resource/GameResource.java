9c881e0dc7b1fb74f6609421d5056633cb0d6f43
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-03-08 17:46:59 +0100

    Refactor

--- src/main/java/org/example/gamesjakartaee/resource/GameResource.java
+++ src/main/java/org/example/gamesjakartaee/resource/GameResource.java
@@ -1,11 +1,13 @@
 package org.example.gamesjakartaee.resource;
 
 import jakarta.inject.Inject;
+import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.example.gamesjakartaee.dto.GameDTO;
-import org.example.gamesjakartaee.repository.GameRepository;
+import org.example.gamesjakartaee.entity.Game;
+import org.example.gamesjakartaee.service.GameService;
 
 import java.net.URI;
 import java.util.List;
@@ -14,34 +16,34 @@ import java.util.List;
 public class GameResource {
 
     @Inject
-    private GameRepository gameRepository;
+    private GameService gameService;
 
     public GameResource() {
     }
 
     @Inject
-    public GameResource(GameRepository gameRepository) {
-        this.gameRepository = gameRepository;
+    public GameResource(GameService gameService) {
+        this.gameService = gameService;
     }
 
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<GameDTO> findAll() {
-        return gameRepository.findAll();
+        return gameService.findAll();
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("{id}")
-    public GameDTO one(@PathParam("id") String id) {
-        return new GameDTO("Mario Kart", 1992);
+    public Game one(@PathParam("id") Long id) {
+        return gameService.findById(id);
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response create(GameDTO gameDTO) {
-        Long id = gameRepository.insertGame(gameDTO);
+    public Response create(@Valid GameDTO gameDTO) {
+        Long id = gameService.insertGame(gameDTO);
         return Response.created(
                         URI.create("http://localhost:8080/games-jakartaEE-1.0-SNAPSHOT/api/games/" + id.toString()))
                 .build();
@@ -50,7 +52,7 @@ public class GameResource {
     @PUT
     @Path("/{id}")
     public Response update(@PathParam("id") Long id, String name){
-        gameRepository.updateGame(id,name);
+        gameService.updateGame(id,name);
         return Response.ok().build();
 
     }
