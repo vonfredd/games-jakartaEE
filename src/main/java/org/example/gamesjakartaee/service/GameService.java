d376162d1b86eeba96b071a52b37a1cd625bd4dc
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-03-08 17:45:17 +0100

    Add service layer

---
+++ src/main/java/org/example/gamesjakartaee/service/GameService.java
@@ -0,0 +1,32 @@
+package org.example.gamesjakartaee.service;
+
+import jakarta.inject.Inject;
+import org.example.gamesjakartaee.dto.GameDTO;
+import org.example.gamesjakartaee.entity.Game;
+import org.example.gamesjakartaee.repository.GameRepository;
+
+import java.util.List;
+
+public class GameService {
+    @Inject
+    GameRepository gameRepository;
+
+
+    public List<GameDTO> findAll() {
+        return gameRepository.findAll();
+    }
+
+    public Long insertGame(GameDTO gameDTO) {
+       return gameRepository.insertGame(GameDTO.map(gameDTO));
+    }
+
+    public void updateGame(Long id, String name) {
+        Game game = gameRepository.findGameById(id);
+        game.setName(name);
+        gameRepository.updateGame(game);
+    }
+
+    public Game findById(Long id) {
+        return gameRepository.findGameById(id);
+    }
+}
