52e2101219873418e487f77c831cb4c190967efb
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-03-12 10:37:28 +0100

    Add init data

---
+++ src/main/resources/META-INF/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Game (title, releaseYear) VALUES ('The Legend of Zelda: Ocarina of Time', 1998);
+INSERT INTO Game (title, releaseYear) VALUES ('Super Mario Bros.', 1985);
+INSERT INTO Game (title, releaseYear) VALUES ('Minecraft', 2011);
+INSERT INTO Game (title, releaseYear) VALUES ('Grand Theft Auto V', 2013);
+INSERT INTO Game (title, releaseYear) VALUES ('The Witcher 3: Wild Hunt', 2015);
