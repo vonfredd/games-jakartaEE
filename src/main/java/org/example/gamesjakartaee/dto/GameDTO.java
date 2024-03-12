af017b29c01a82e953ef1bfe221f426eb5bca84b
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-03-12 10:38:35 +0100

    Rename name to title

--- src/main/java/org/example/gamesjakartaee/dto/GameDTO.java
+++ src/main/java/org/example/gamesjakartaee/dto/GameDTO.java
@@ -4,14 +4,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PositiveOrZero;
 import org.example.gamesjakartaee.entity.Game;
 
-public record GameDTO(@NotBlank String name, @PositiveOrZero int releaseYear) {
+public record GameDTO(@NotBlank String title, @PositiveOrZero int releaseYear) {
     public static GameDTO map (Game game){
-        return new GameDTO(game.getName(), game.getReleaseYear());
+        return new GameDTO(game.getTitle(), game.getReleaseYear());
     }
 
     public static Game map (GameDTO gameDTO){
         var game = new Game();
-        game.setName(gameDTO.name);
+        game.setTitle(gameDTO.title);
         game.setReleaseYear(gameDTO.releaseYear);
         return game;
     }
